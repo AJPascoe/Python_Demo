a8bcf92cee7e4a90bfafd2ef9956c46f28d7d3d6
Adam Pascoe <adampascoe@adviserplus.com>
2023-06-27 11:25:17 +0100

    adding files

---
+++ conditionals.py
@@ -0,0 +1,60 @@
+## if statement ##
+
+# my_bool = False
+
+# if my_bool:
+#     print("My bool is true")
+# else:
+#     print("My bool is false")
+
+
+## Nested if  ##
+
+# money = 9
+
+# if money > 10:
+#     print("I have some money")
+# else:
+#     print("I don't have much money")
+
+
+## elif statments - else if ##
+# used to only trigger one statement
+
+# temp = 28
+
+# if temp >= 30:
+#     print("It's very hot today")
+# elif temp>= 25:
+#     print("Its pretty hot")
+# elif temp > 20:
+#     print("It's ok")
+# elif temp > 15:
+#     print("It could be better")
+# elif temp == 0:
+#     print("It is going to freeze")
+# else:
+#     print("normal day today")
+
+## Exercise 1 ##
+
+# grade = int(input("Please enter your grade: "))
+
+# if grade > 85:
+#     print("Distinction")
+# elif grade >= 65:
+#     print("Pass")
+# else:
+#     print("Fail")
+
+## Exercise 2 ##
+
+# x = input("kgs (k) or lbs(l): ")
+# y = int(input("Please enter the wieght: "))
+
+# if x == "k":
+#     print("You have selected kgs")
+#     print("The weight is ",y,"kgs")
+# elif x == "l":
+#     print("You selected lbs")
+#     print ("The weight is ",y / 2.2,"kgs")
---
+++ vars+data_types.py
@@ -0,0 +1,32 @@
+# this line will be ignored
+# Variables are references
+
+
+#x = 10
+#print(x)
+
+# age = input("Enter your age: ")
+# age1 = int(input("Enter your age: "))
+
+# print("Your age is ", age)
+# print(type(age))
+# print(type(age1))
+
+### PART 1 ###
+print('Hello World!')
+
+### PART 2 ###
+#name='Adam'
+#age=26
+#print(name,'is',age,'years old')
+
+#name=input('Please enter your name: ')
+#age = input('Please enter your age: ')
+#print(name,'is',age,'years old')
+
+### PART 3 ###
+length1 = int(input('Length of hight in cm: '))
+length2 = int(input('Length of width in cm: '))
+
+print('The parimiter of the rectangle is ',length1 + length1 + length2 + length2)
+print('The area of the rentangle is ',length1 * length2)
