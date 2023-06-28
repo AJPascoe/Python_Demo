8c7751bbb5e7da16c08a0f281bf6407533171d1d
Adam Pascoe <adampascoe@adviserplus.com>
2023-06-28 15:47:56 +0100

    adding files

--- lab4/countVowels.py
+++ lab4/countVowels.py
@@ -8,12 +8,12 @@
 #     c += 1  # updates counter with the amount of vowels from vc
 # print(vc)
 
-word = "hello"
+word = input(": ")
 vc = 0
 c= 0
 for l in word:
     if l.lower() in "aeiou":   # checks to see if any of the letters are vowels
         vc += 1  # if so add 1 to vowel counter
         c += 1  # updates counter with the amount of vowels from vc
-print(vc)
+print(f"{vc} vowels in this string")
 
