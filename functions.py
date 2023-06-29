098ba605cbf51276d6fcc1da04f76b83b8ae772e
Adam Pascoe <adampascoe@adviserplus.com>
2023-06-29 11:38:45 +0100

    adding files

---
+++ functions.py
@@ -0,0 +1,15 @@
+
+
+# def calc(x,y):
+#     i = x+y
+#     return i
+    
+# print(calc(5,10))
+
+# def addNum(x,y,z) :
+#     i = x + y + z
+#     return i
+# num = addNum(1,2,3)
+# print("the total sum value is : ", num)
+
+
